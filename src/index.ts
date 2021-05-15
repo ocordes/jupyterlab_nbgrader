9c8a46cbe148e68e8d8b5cfad60e7d57cf8d9ae0
Oliver Cordes <ocordes@astro.uni-bonn.de>
2021-05-15 17:24:40 +0200

    - demo is working

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,70 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+  IDisposable, DisposableDelegate
+} from '@lumino/disposable';
+
+
+import {
+  ToolbarButton
+} from '@jupyterlab/apputils';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+import {
+  NotebookActions, NotebookPanel, INotebookModel
+} from '@jupyterlab/notebook';
+
+
+
+/**
+ * A notebook widget extension that adds a button to the toolbar.
+ */
+export
+  class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  /**
+   * Create a new extension object.
+   */
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+    let callback = () => {
+      NotebookActions.runAll(panel.content, context.sessionContext);
+    };
+    let button = new ToolbarButton({
+      className: 'myButton',
+      iconClass: 'fa fa-fast-forward',
+      onClick: callback,
+      tooltip: 'Run All'
+    });
+
+    panel.toolbar.insertItem(0, 'runAll', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
+}
+
+
+/**
+ * Activate the extension.
+ */
+function activate(app: JupyterFrontEnd) {
+  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+};
+
+
 /**
  * Initialization data for the jupyterlab_nbgrader extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_nbgrader:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab_nbgrader is activated!');
-  }
+  activate: activate
+  //activate: (app: JupyterFrontEnd) => {
+  //  console.log('JupyterLab extension jupyterlab_nbgrader is activated!');
+  //}
 };
 
 export default extension;
