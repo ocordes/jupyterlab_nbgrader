6de3d3f475911dc355c96dd5aee46309cd430553
Oliver Cordes <ocordes@astro.uni-bonn.de>
2021-05-27 16:17:59 +0200

    - first working version

--- src/index.ts
+++ src/index.ts
@@ -3,59 +3,17 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import {
-  IDisposable, DisposableDelegate
-} from '@lumino/disposable';
+import '../style/index.css';
 
-
-import {
-  ToolbarButton
-} from '@jupyterlab/apputils';
-
-import {
-  DocumentRegistry
-} from '@jupyterlab/docregistry';
-
-import {
-  NotebookActions, NotebookPanel, INotebookModel
-} from '@jupyterlab/notebook';
-
-
-
-/**
- * A notebook widget extension that adds a button to the toolbar.
- */
-export
-  class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
-  /**
-   * Create a new extension object.
-   */
-  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
-    let callback = () => {
-      NotebookActions.runAll(panel.content, context.sessionContext);
-    };
-    let button = new ToolbarButton({
-      className: 'myButton',
-      iconClass: 'fa fa-fast-forward',
-      onClick: callback,
-      tooltip: 'Run All'
-    });
-
-    panel.toolbar.insertItem(0, 'runAll', button);
-    return new DisposableDelegate(() => {
-      button.dispose();
-    });
-  }
-}
-
+import { ValidationButton } from './validate_assignment';
 
 /**
  * Activate the extension.
  */
 function activate(app: JupyterFrontEnd) {
-  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
-};
-
+  app.docRegistry.addWidgetExtension('Notebook', new ValidationButton());
+  console.debug('JupyterLab extension jupyterlab_nbgrader is activated!');
+}
 
 /**
  * Initialization data for the jupyterlab_nbgrader extension.
@@ -64,9 +22,6 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_nbgrader:plugin',
   autoStart: true,
   activate: activate
-  //activate: (app: JupyterFrontEnd) => {
-  //  console.log('JupyterLab extension jupyterlab_nbgrader is activated!');
-  //}
 };
 
 export default extension;
---
+++ src/validate_assignment.ts
@@ -0,0 +1,160 @@
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
+
+import { Widget } from '@lumino/widgets';
+
+import { ToolbarButton, Dialog, showDialog } from '@jupyterlab/apputils';
+
+import { DocumentRegistry } from '@jupyterlab/docregistry';
+
+import {
+  //NotebookActions,
+  NotebookPanel,
+  INotebookModel
+} from '@jupyterlab/notebook';
+
+import { requestAPI } from './handler';
+
+const nbgrader_version = '0.7.0.dev';
+
+function validate(data: any) {
+  const body = document.createElement('div');
+  body.id = 'validation-message';
+  if (data.success === true) {
+    if (typeof data.value === 'string') {
+      data = JSON.parse(data.value);
+    } else {
+      data = data.value;
+    }
+    if (data.type_changed !== undefined) {
+      for (let i = 0; i < data.type_changed.length; i++) {
+        const cell = document.createElement('div');
+        cell.innerHTML =
+          '<p>The following ' +
+          data.type_changed[i].old_type +
+          ' cell has changed to a ' +
+          data.type_changed[i].new_type +
+          ' cell, but it should not have!</p>';
+        body.append(cell);
+        const source = document.createElement('pre');
+        source.append(data.type_changed[i].source);
+        body.append(source);
+      }
+      body.className = 'validation-type-changed';
+    } else if (data.changed !== undefined) {
+      for (let i = 0; i < data.changed.length; i++) {
+        const cell = document.createElement('div');
+        cell.innerHTML =
+          '<p>The source of the following cell has changed, but it should not have!</p>';
+        body.append(cell);
+        const source = document.createElement('pre');
+        source.append(data.type_changed[i].source);
+        body.append(source);
+      }
+      body.className = 'validation-changed';
+    } else if (data.passed !== undefined) {
+      for (let i = 0; i < data.changed.length; i++) {
+        const cell = document.createElement('div');
+        cell.innerHTML = '<p>The following cell passed:</p>';
+        body.append(cell);
+        const source = document.createElement('pre');
+        source.append(data.type_changed[i].source);
+        body.append(source);
+      }
+      body.className = 'validation-passed';
+    } else if (data.failed !== undefined) {
+      for (let i = 0; i < data.failed.length; i++) {
+        const cell = document.createElement('div');
+        cell.innerHTML = '<p>The following cell failed:</p>';
+        body.append(cell);
+        const source = document.createElement('pre');
+        source.append(data.failed[i].source);
+        body.append(source);
+        const error = document.createElement('pre');
+        error.innerHTML = data.failed[i].error;
+        body.append(error);
+      }
+      body.className = 'validation-failed';
+    } else {
+      const cell = document.createElement('div');
+      cell.innerHTML = '<p>Success! Your notebook passes all the tests.</p>';
+      body.append(cell);
+      body.className = 'validation-success';
+    }
+  }
+  return body;
+}
+
+/**
+ * A notebook widget extension that adds a button to the toolbar.
+ */
+export class ValidationButton
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
+{
+  /**
+   * Create a new extension object.
+   */
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+    const callback = () => {
+      context.save().then(async () => {
+        //console.log(button);
+        // is called when saving was successfull!
+
+        const check_version = await requestAPI<any>(
+          'nbgrader_version',
+          {},
+          { version: nbgrader_version }
+        ).then(values => {
+          return values;
+        });
+
+        // check if the nbgrader version is compatible
+        if (!check_version.success) {
+          const body = document.createElement('div');
+          body.innerHTML = '<p>' + check_version.message + '</p>';
+          showDialog({
+            title: 'Version Mismatch',
+            body: new Widget({ node: body }),
+            focusNodeSelector: 'input',
+            buttons: [Dialog.okButton()]
+          }); //.then(() => {});
+          return;
+        }
+
+        console.log('validating the notebook');
+        const data = new URLSearchParams();
+        data.append('path', context.path);
+        await requestAPI<any>('assignments/validate', {
+          method: 'POST',
+          body: data
+        }).then(values => {
+          const body = validate(values);
+          showDialog({
+            title: 'Validation Results',
+            body: new Widget({ node: body }),
+            focusNodeSelector: 'input',
+            buttons: [Dialog.okButton()]
+          }).then(result => {
+            console.log(result);
+          });
+          return values;
+        });
+      });
+    };
+
+    const button = new ToolbarButton({
+      label: 'Validate',
+      className: 'myButton',
+      onClick: callback,
+      tooltip: 'Validate Notebook'
+    });
+
+    //panel.toolbar.insertItem(0, 'jp-nb-validate', button);
+    panel.toolbar.insertAfter('cellType', 'jp-nb-validate', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
+}
